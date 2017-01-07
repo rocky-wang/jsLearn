d70941fcde24e9fd7f6ab502b9ed9591593028ee
Rocky-Wang <eleyuan@163.com>
2017-01-07 19:53:58 +0800

    Dom表格操作应用：查找、排序、表样式

--- 表格Dom操作/tableOps.js
+++ 表格Dom操作/tableOps.js
@@ -2,11 +2,14 @@
  * Created by Rocky-Wang on 2017/1/6.
  */
 function setColorByRules(oTable) {
-    console.log('set Color by rules: ' + oTable.tBodies[0].rows.length);
+    // 遍历表格中的每一行
     for (var i = 0; i < oTable.tBodies[0].rows.length; i++){
         var oRow = oTable.tBodies[0].rows[i];
         if (i % 2){
             oRow.style.backgroundColor = '#ccc';
+        }
+        else {
+            oRow.style.backgroundColor = '';
         }
 
         var oldColor = '';
@@ -53,6 +56,61 @@ function addTableByInputData(oTable) {
     };
 
     oTable.tBodies[0].appendChild(oTr);
+
+    setColorByRules(oTable);
+}
+
+function searchByText(oTable, sContent) {
+    setColorByRules(oTable);
+    for (var i = 0; i < oTable.tBodies[0].rows.length; i++){
+        var oCell = oTable.tBodies[0].rows[i].cells[1];
+
+        if (oCell.innerHTML === sContent){
+            oCell.parentNode.style.backgroundColor = 'yellow';
+        }
+    }
+}
+
+function sortList(oUl) {
+    var aLis = oUl.getElementsByTagName('li');
+
+    var arrayBuf = [];
+
+    for (var i = 0; i < aLis.length; i++){
+        arrayBuf[i] = aLis[i];
+    }
+
+    arrayBuf.sort(function (li1, li2) {
+        var n1 = parseInt(li1.innerHTML);
+        var n2 = parseInt(li2.innerHTML);
+
+        return n1 - n2;
+    });
+
+    for (i = 0; i < arrayBuf.length; i++){
+        oUl.appendChild(arrayBuf[i]);
+    }
+
+}
+
+function sortByScore(oTable) {
+    var arrayRows = [];
+    var aElementTrs = oTable.tBodies[0].rows;
+
+    for (var i = 0; i < aElementTrs.length; i++){
+        arrayRows[i] = aElementTrs[i];
+    }
+
+    arrayRows.sort(function (aTr1, aTr2) {
+       var n1 = parseInt(aTr1.cells[3].innerHTML);
+       var n2 = parseInt(aTr2.cells[3].innerHTML);
+
+       return n2 - n1;
+    });
+
+    for (i = 0; i < arrayRows.length; i++){
+        oTable.tBodies[0].appendChild(arrayRows[i]);
+    }
 
     setColorByRules(oTable);
 }
